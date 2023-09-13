6c3bd2d75aa6c0e09f0d5aa72eb018e61d98e113
Kavya Singhal <singhal.kavya03@gmail.com>
2023-09-14 00:06:28 +0530

    Ready for Deployment v3

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-      origin: "http://localhost:3000",
+      origin: "https://vercel.com/cdc-placeminds/placeminds-frontend/A278W3Pg8RpfbgQVvWMyWocjxJYT",
       credentials: true,
     })
   );
