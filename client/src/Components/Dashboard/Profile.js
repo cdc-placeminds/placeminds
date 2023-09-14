f150d2f5f5b0671f7d7c9484f2ba896b07b76b2b
Kavya Singhal <singhal.kavya03@gmail.com>
2023-09-14 15:53:35 +0530

    Ready for Deployment v25

--- client/src/Components/Dashboard/Profile.js
+++ client/src/Components/Dashboard/Profile.js
@@ -169,7 +169,7 @@ const Profile = () => {
       <div className="col-md-4 mt-[2%]">
 
         {/* profile div  */}
-        <div className='relative z-[100] grid grid-flow-row grid-cols-2 bg-white h-[37vh] md:h-[42vh] p-[2%] mt-[1%] border-1 border-solid border-cardborder rounded-xl'>
+        <div className= {`relative ${cardbtnclkd ? 'z-[10]' : 'z-[100]'} grid grid-flow-row grid-cols-2 bg-white h-[37vh] md:h-[42vh] p-[2%] mt-[1%] border-1 border-solid border-cardborder rounded-xl`}>
 
 
           <div className="profimgdiv flex flex-col items-center justify-center ">
