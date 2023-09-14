11291163af561f572eab8a2cfa7593462e29d1ca
Kavya Singhal <singhal.kavya03@gmail.com>
2023-09-14 12:55:39 +0530

    Ready for Deployment v20

--- server/index.js
+++ server/index.js
@@ -24,13 +24,20 @@ connection();
 //Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://placeminds-frontend.vercel.app/');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials
-  next();
-});
+
+app.use(cors({
+  'Access-Control-Allow-Origin': ['https://placeminds-frontend.vercel.app',''],
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  'Access-Control-Allow-Credentials': 'true'
+}));
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'https://placeminds-frontend.vercel.app');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials
+//   next();
+// });
 
 //Routes
 app.use("/api/users", userRoutes);
